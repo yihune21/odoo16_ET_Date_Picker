eaf5bbe2dfdf4f53c5e946178d2cf00e8237f242
yihune21 <zewdieyihune@gmail.com>
2024-08-30 19:57:04 +0300

    commit

---
+++ models/purchase_order.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api
+from ethiopian_date import EthiopianDateConverter
+from datetime import datetime
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    purchase_date_et = fields.Char(string='Purchase Date (ET)', compute='_compute_et_date',store=True)
+
+
+    @api.depends('date_order')
+    def _compute_et_date(self):
+        for order in self:
+            if order.date_order:
+                try:
+                    order_date = order.date_order.date() if isinstance(order.date_order, datetime) else order.date_order
+                    et_date_tuple = EthiopianDateConverter.date_to_ethiopian(order_date)
+                    order.purchase_date_et = f"{et_date_tuple[2]:02d}/{et_date_tuple[1]:02d}/{et_date_tuple[0]}"
+                except Exception:
+                    order.purchase_date_et = False
+            else:
+                order.purchase_date_et = False
+
+
+    @api.onchange('purchase_date_et')
+    def _onchange_purchase_date_et(self):
+        for order in self:
+            if order.purchase_date_et:
+                try:
+                    day, month, year = map(int, order.purchase_date_et.split('/'))
+                    gregorian_date = EthiopianDateConverter.to_gregorian(year, month, day)
+                    order.date_order = fields.Datetime.to_string(datetime.combine(gregorian_date, datetime.min.time()))
+                except Exception as e:
+                    _logger.error(f"Error converting Ethiopian date: {e}")
---
+++ static/src/js/et_date_picker.js
@@ -0,0 +1,172 @@
+// @odoo-module
+import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { Component, useState, onWillStart, onMounted,useEffect } from "@odoo/owl";
+import { EthiopianDate } from "./ethiopian_date";
+
+class DateTimeEthiopiaField extends Component {
+    static template = 'ethiopian_date_picker.DateTimeEthiopiaField';
+    static props = { ...standardFieldProps };
+
+    setup() {
+
+        this.ETHIOPIAN_MONTHS = [
+            "መስከረም", "ጥቅምት", "ህዳር", "ታህሳስ", "ጥር", "የካቲት",
+            "መጋቢት", "ሚያዝያ", "ግንቦት", "ሰኔ", "ሐምሌ", "ነሐሴ", "ጳጉሜ"
+        ];
+        this.ETHIOPIAN_DAYS = ["እሁድ", "ሰኞ", "ማክሰኞ", "እሮብ", "ሐሙስ", "አርብ", "ቅዳሜ"];
+
+        this.state = useState({
+            inputValue: "",
+            showCalendar: false,
+            currentYear: 0,
+            currentMonth: 0,
+            currentDay: 0,
+            selectedDate: null,
+        });
+
+        onWillStart(async () => {
+            this.initializeDate();
+        });
+
+        onMounted(() => {
+            this.formatDate();
+        });
+
+         useEffect(() => {
+                this.syncWithOdooField();
+        });
+
+    }
+
+
+    initializeDate=()=> {
+        const today = new Date();
+        const ethiopianDate = EthiopianDate.fromGregorian(today);
+        this.state.currentYear = ethiopianDate.year;
+        this.state.currentMonth = ethiopianDate.month - 1;
+        this.state.currentDay = ethiopianDate.day;
+        this.state.inputValue = ethiopianDate.format();
+    }
+
+    formatDate=()=> {
+        if (this.props.value) {
+            const date = new Date(this.props.value);
+            if (!isNaN(date.getTime())) {
+                const ethiopianDate = EthiopianDate.fromGregorian(date);
+                this.state.inputValue = ethiopianDate.format();
+                this.state.selectedDate = ethiopianDate;
+            } else {
+                this.resetState();
+            }
+        } else {
+            this.resetState();
+        }
+    }
+
+    resetState=()=> {
+        this.state.inputValue = '';
+        this.state.selectedDate = null;
+        this.state.currentYear = new Date().getFullYear();
+        this.state.currentMonth = new Date().getMonth();
+    }
+
+    onInputClick=()=>{
+        this.state.showCalendar = !this.state.showCalendar;
+    }
+
+    changeMonth=(delta) => {
+        let newMonth = this.state.currentMonth + delta;
+        let newYear = this.state.currentYear;
+
+        if (newMonth > 12) {
+            newMonth = 0;
+            newYear++;
+        } else if (newMonth < 0) {
+            newMonth = 12;
+            newYear--;
+        }
+
+
+        this.state.currentMonth = newMonth;
+        this.state.currentYear = newYear;
+        this.state.showCalendar = true;
+    }
+
+  syncWithOdooField() {
+    const odooFieldValue = this.props.value;
+    console.log('odooFieldValue:', odooFieldValue);
+
+    if (odooFieldValue && (!this.state.selectedDate || this.state.selectedDate.toGregorian().toISOString() !== odooFieldValue)) {
+        // Parse the date string
+        const [day, month, year] = odooFieldValue.split('/');
+        const gregorianDate = new EthiopianDate(year,month,day).toGregorian()
+        // Check if the gregorianDate is valid
+        if (!isNaN(gregorianDate.getTime())) {
+            try {
+                const ethiopianDate = EthiopianDate.fromGregorian(gregorianDate);
+                // Check if ethiopianDate is valid and has the required properties
+                if (ethiopianDate && ethiopianDate.year && ethiopianDate.month && ethiopianDate.day) {
+                        this.state.inputValue=ethiopianDate.format();
+                        this.state.selectedDate=ethiopianDate;
+                        this.state.currentYear=ethiopianDate.year;
+                        this.state.currentMonth=ethiopianDate.month - 1;
+                } else {
+                    console.error('Invalid Ethiopian date');
+                }
+            } catch (error) {
+                console.error('Error converting to Ethiopian date:', error);
+            }
+        } else {
+            console.error('Invalid Gregorian date:', odooFieldValue);
+        }
+    }
+}
+
+    onDateSelect=(day) => {
+       const selectedDate = new EthiopianDate(this.state.currentYear, this.state.currentMonth + 1, day);
+        const formattedEthiopianDate = selectedDate.format();
+
+        this.state.inputValue = formattedEthiopianDate;
+        this.state.selectedDate = selectedDate;
+        this.state.showCalendar = false;
+
+        // Update the Ethiopian date field
+        this.props.update(formattedEthiopianDate);
+
+        // Trigger onchange to update Odoo field
+        if (this.props.onchange) {
+            this.props.onchange(formattedEthiopianDate);
+        }
+
+    }
+
+
+    getDaysInMonth=(year, month)=> {
+        if (month < 12) {
+            return Array.from({ length: 30 }, (_, i) => i + 1);
+        } else {
+            return Array.from({ length: year % 4 === 3 ? 6 : 5 }, (_, i) => i + 1);
+        }
+    }
+
+    isToday=(day)=> {
+        const today = new Date();
+        const ethiopianToday = EthiopianDate.fromGregorian(today);
+        return (
+            ethiopianToday.year === this.state.currentYear &&
+            ethiopianToday.month === this.state.currentMonth + 1 &&
+            ethiopianToday.day === day
+        );
+    }
+
+    getDayOfWeek=(year, month, day)=> {
+        const ethiopianDate = new EthiopianDate(year, month + 1, day);
+        const gregorianDate = ethiopianDate.toGregorian();
+        return gregorianDate.getDay();
+    }
+
+
+}
+
+registry.category("fields").add("date_et", DateTimeEthiopiaField);
